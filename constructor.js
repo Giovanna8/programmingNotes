18976bc2cf510a66f73dd27b99e568fcfaba236c
Manny Hagman <manny.hagman@gmail.com>
2016-10-21 21:31:18 -0500

    added constructor.js file and made slight change to constructor markdown file

---
+++ constructor.js
@@ -0,0 +1,19 @@
+//just adding a few things that might go with the constructor function chapter//
+//in javascript constructors function names usually begin with a capital letter//
+
+// Object Literals
+
+'use strict';
+
+var manny = {
+    firstName: 'Manny',
+    lastName: 'Hagman',
+    age: 35
+};
+
+console.log(manny);
+console.log(manny.firstName);
+console.log(manny.lastName);
+console.log(manny.age);
+
+//Object literals are used when you want to create a single Object and there wil be little to no reuse of the code later in the application.
