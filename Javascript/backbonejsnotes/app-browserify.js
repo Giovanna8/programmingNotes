4b29068e8f98dfe9971e93711009b36ea0e6f2f1
materialfuture <materialfuture@gmail.com>
2016-11-30 19:16:38 -0600

    added git hub notes | don't hate me

--- Javascript/backbonejsnotes/app-browserify.js
+++ Javascript/backbonejsnotes/app-browserify.js
@@ -2,7 +2,7 @@
 
 // es5 polyfills, powered by es5-shim
 require("es5-shim")
-    // es6 polyfills, powered by babel
+// es6 polyfills, powered by babel
 require("babel/register")
 
 var Promise = require('es6-promise').Promise
